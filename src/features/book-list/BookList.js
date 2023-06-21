c74d63736d702d2e9ff33c654a53be66f3b62014
COil <vernet.loic@gmail.com>
2023-06-21 15:56:33 +0200

    book: list with component + sort

--- src/features/book-list/BookList.js
+++ src/features/book-list/BookList.js
@@ -1,3 +1,6 @@
+import './BookList.css';
+import {useState} from 'react';
+
 const books = [
     {isbn: '9793083416011', title: 'Excepturi explicabo volu…am aut voluptate velit.', author: 'Corbin Ritchie'},
     {isbn: '9786451149666', title: 'Est est et eveniet porro non.', author: 'Deonte Effertz'},
@@ -11,16 +14,46 @@ const books = [
 ];
 
 const BookList = () => {
+    const [order, setOrder] = useState(null);
+
+    const orderedBooks = order === null ?
+        books :
+        books.sort((a, b) => {
+            return a[order] < b[order] ? -1 :
+                a[order] > b[order] ? 1 : 0;
+        });
+
     return (
         <>
-            <h1>Livres</h1>
+            <header className="book-list-header">
+                <h1>Livres</h1>
+                <p className="book-list-order">
+                    Trier par
+                    <button onClick={() => setOrder('isbn')}>isbn</button> -
+                    <button onClick={() => setOrder('title')}>titre</button> -
+                    <button onClick={() => setOrder('author')}>auteur</button>
+                </p>
+            </header>
+            <div className="book-list">
                 <ul>
-                    { books.map(book => (
-                        <li>{ book.isbn } { book.author } { book.title } </li>
+                    { orderedBooks.map(book => (
+                        <Book key={book.isbn} book={book} />
                     )) }
                 </ul>
+            </div>
         </>
     );
 };
+
+const Book = ({book}) => {
+    return (
+        <div className="book-card">
+            <img src={`https://picsum.photos/seed/${book.isbn}/300/400`} width="100px" alt={book.title} />
+            <h3>{book.title}</h3>
+            <p>Auteur⋅rice : {book.author}</p>
+            <p>Isbn : {book.isbn}</p>
+        </div>
+    );
+}
 
 export default BookList;
